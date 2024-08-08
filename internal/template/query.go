016c2e5da5d6a1a88f0897a2b92b45b7646cc75d
Lazar Nikolić <lazarpwna@gmail.com>
2024-08-08 16:28:27 +0200

    fix: Fix Query field names

--- internal/template/query.go
+++ internal/template/query.go
@@ -67,7 +67,7 @@ func (q *Query) ReplaceDB(db *gorm.DB) *Query {
 
 type queryCtx struct{ 
 	{{range $name,$d :=.Data -}}
-	{{$d.ModelStructName}}Exported {{$d.ReturnObject}}
+	{{$d.ModelStructName}} {{$d.ReturnObject}}
 	{{end}}
 }
 
